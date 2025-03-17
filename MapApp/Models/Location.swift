07b8ae5c7ce6e4d5cc71a7ffe43b622f58d393c4
Rusln <cpptypxe@ya.ru>
2025-03-16 17:15:38 -0700

    [Feature] New Model file and Assets

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by dofxmine on 16.03.2025.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
